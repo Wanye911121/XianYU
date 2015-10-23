374dee224785dc07ac659d20bb19eefb26df0c35
ZpyZp <zhangpy@zpyzpdemacbook-air.local>
2015-10-23 16:04:31 +0800

    Initial Commit

---
+++ XianYu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XianYu
+//
+//  Created by ZpyZp on 15/10/23.
+//  Copyright © 2015年 berchina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XianYu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XianYu
+//
+//  Created by ZpyZp on 15/10/23.
+//  Copyright © 2015年 berchina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
